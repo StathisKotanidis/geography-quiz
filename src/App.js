6b03bf887c3b3525d78a4e61d771a5d8dc00d105
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-09-29 17:35:33 +0300

    starting screen done

--- src/App.js
+++ src/App.js
@@ -10,9 +10,32 @@ export default function App() {
 
 function StartingScreen() {
   return (
-    <div>
-      <h1> 🌍 Ultimate Geography Quiz</h1>
-      <button>Start</button>
+    <div className="starting-screen-container">
+      <div className="quiz-title-container">
+        <h1> 🌍 Ultimate Geography Quiz</h1>
+      </div>
+      <div className="quiz-description-container">
+        <div className="description-container">
+          <p style={{ color: "yellow" }} className="typing">
+            4 unique challenges
+          </p>
+        </div>
+
+        <div className="description-container">
+          <p style={{ color: "blue" }} className="typing">
+            40 different questions
+          </p>
+        </div>
+
+        <div className="description-container">
+          <p style={{ color: "green" }} className="typing">
+            Are you ready to play?
+          </p>
+        </div>
+      </div>
+      <div className="start-button-container">
+        <button className="startBtn">Start</button>
+      </div>
     </div>
   );
 }
